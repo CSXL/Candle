4737af4e1d0a9e9dba72d15b7455718a9bb2d229
ecsbeats <adamb@csxlabs.org>
2022-12-03 22:42:37 -0800

    clean slate

--- main.go
+++ main.go
@@ -1,65 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/CSXL/Candle/api"
-	input "github.com/CSXL/Candle/util/input"
-)
-
-// We can mock these functions in our tests
-var osExit = os.Exit
-var NewScanner input.ScannerInterface = input.NewScanner()
-
-// When true the main function will exit after the first command
-var TESTING_MODE = false
+import "fmt"
 
 func main() {
-	var COMMAND_PROVIDED = false
-	if len(os.Args) > 1 {
-		COMMAND_PROVIDED = true
-	}
-
-	scanner := NewScanner
-
-	for {
-		fmt.Print("--> ")
-		var args []string
-		if COMMAND_PROVIDED {
-			args = os.Args[1:]
-		} else {
-			scanner.Scan()
-			args = strings.Split(scanner.Text(), " ")
-		}
-
-		switch args[0] {
-		case "about":
-			fmt.Println("Candle CLI")
-			fmt.Println("CSX Labs")
-			fmt.Println("Made w/ <3 by @absozero and @ecsbeats")
-		case "get-info":
-			stock_data := api.GetStockData(args[1])
-			// Prints first 10 records
-			var i int = 0
-			for key, value := range stock_data {
-				if i == 10 {
-					break
-				}
-				fmt.Println(key, value)
-				i++
-			}
-
-		case "exit":
-			fmt.Println("Exiting...")
-			osExit(0)
-		default:
-			fmt.Printf("Invalid command: %s\n", args[0])
-		}
-
-		if COMMAND_PROVIDED || TESTING_MODE {
-			break
-		}
-	}
+	fmt.Println("Hello, world!")
 }
--- main_test.go
+++ main_test.go
@@ -1,79 +1,9 @@
 package main
 
 import (
-	"io"
-	"os"
 	"testing"
-
-	input "github.com/CSXL/Candle/util/input"
-	"github.com/stretchr/testify/assert"
 )
 
-// Function to capture standard output from the main function with the given os.Args
-func CaptureStandardOutput(os_args ...string) []byte {
-	os.Args = os_args
-	r, w, _ := os.Pipe()
-	os.Stdout = w
+func TestMain(t *testing.T) {
 	main()
-	w.Close()
-	out, _ := io.ReadAll(r)
-	os.Stdout = os.Stderr
-	return out
-}
-
-var original_arguments []string
-
-func TestMain(m *testing.M) {
-	original_arguments = os.Args // Some tests need to access the original arguments to re-run the test command.
-	os.Exit(m.Run())
-}
-
-func TestCommandUsingInput(t *testing.T) {
-	// Utilize the input package to mock the Scanner function
-	NewScanner = input.NewMockScanner()
-	NewScanner.WriteString("about")
-	os.Args = []string{"candle"}
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-	TESTING_MODE = true // So that the main function exits after the command
-	main()
-	w.Close()
-	out, _ := io.ReadAll(r)
-	os.Stdout = os.Stderr
-	assert.Equal(t, "--> Candle CLI\nCSX Labs\nMade w/ <3 by @absozero and @ecsbeats\n", string(out))
-}
-
-// Test the get-info command
-func TestGetInfo(t *testing.T) {
-	var out = CaptureStandardOutput("candle", "get-info", "IBM")
-	assert.NotEmpty(t, out)
-}
-
-// Test the about command
-func TestAbout(t *testing.T) {
-	var out = CaptureStandardOutput("candle", "about")
-	assert.Equal(t, "--> Candle CLI\nCSX Labs\nMade w/ <3 by @absozero and @ecsbeats\n", string(out))
-}
-
-// Test the exit command
-func TestExit(t *testing.T) {
-	// Save current function and restore at the end:
-	oldOsExit := osExit
-	defer func() { osExit = oldOsExit }()
-
-	var exit_code int
-	mockExit := func(code int) {
-		exit_code = code
-	}
-
-	osExit = mockExit
-	os.Args = []string{"candle", "exit"}
-	main()
-	assert.Equal(t, 0, exit_code, "Exit code should be 0, but was %d", exit_code)
-}
-
-// Test an invalid command
-func TestInvalidCommand(t *testing.T) {
-	var out = CaptureStandardOutput("candle", "veryinvalidcommand")
-	assert.Equal(t, "--> Invalid command: veryinvalidcommand\n", string(out))
 }
